025e28e1b0e622f443d09628c16baa1d02f5f3d2
Karthikeya Kotha <kkotha2@wisc.edu>
2025-11-22 17:41:11 -0600

    First commit for ECE752 project - Stable build

---
+++ gem5/src/arch/x86/insts/microavxop.cc
@@ -0,0 +1,10 @@
+// All AVXOpBase inline function implementations have been moved to microavxop.hh
+// This file is intentionally minimal to avoid multiple definition errors.
+
+#include "arch/x86/insts/microavxop.hh"
+
+namespace gem5 { namespace X86ISA {
+
+// Placeholder for any future non-inline AVXOpBase member functions
+
+} } // namespace gem5::X86ISA
