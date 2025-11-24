de95bbdb5aab5d6a2acf527ed74a02b7648e8cbf
Karthikeya Kotha <kkotha2@wisc.edu>
2025-11-23 20:26:47 -0600

    Figured out the working of AVX

--- gem5/src/arch/x86/isa/insts/avx_subset/floating_point/arithmetic/vaddps.py
+++ gem5/src/arch/x86/isa/insts/avx_subset/floating_point/arithmetic/vaddps.py
@@ -4,10 +4,10 @@ def macroop VADDPS_XMM_XMM {
     vaddf dest=xmm0, src1=xmm0v, src2=xmmrm, size=4, VL=16
     vclear dest=xmm2, destVL=16
 };
-# def macroop VADDPS_XMM_XMM_XMM_XMM {
-#     vaddf dest=xmm0, src1=xmm0v, src2=xmmrm, size=4, VL=16
-#     vclear dest=xmm2, destVL=16
-# };
+def macroop VADDPS_XMM_XMM_XMM_XMM {
+    vaddf dest=xmm0, src1=xmm0v, src2=xmmrm, size=4, VL=16
+    vclear dest=xmm2, destVL=16
+};
 def macroop VADDPS_XMM_M {
     ldfp ufp0, seg, sib, "DISPLACEMENT", dataSize=8
     ldfp ufp1, seg, sib, "DISPLACEMENT + 8", dataSize=8
@@ -26,24 +26,24 @@ def macroop VADDPS_XMM_XMM_P {
     vaddf dest=xmm0, src1=xmm0v, src2=ufp0, size=4, VL=16
     vclear dest=xmm2, destVL=16
 };
-# def macroop VADDPS_XMM_XMM_XMM_M {
-#     ldfp ufp0, seg, sib, "DISPLACEMENT", dataSize=8
-#     ldfp ufp1, seg, sib, "DISPLACEMENT + 8", dataSize=8
-#     vaddf dest=xmm0, src1=xmm0v, src2=ufp0, size=4, VL=16
-#     vclear dest=xmm2, destVL=16
-# };
-# def macroop VADDPS_XMM_XMM_XMM_P {
-#     ldfp ufp0, seg, sib, "DISPLACEMENT", dataSize=8
-#     ldfp ufp1, seg, sib, "DISPLACEMENT + 8", dataSize=8
-#     vaddf dest=xmm0, src1=xmm0v, src2=ufp0, size=4, VL=16
-#     vclear dest=xmm2, destVL=16
-# };
+def macroop VADDPS_XMM_XMM_XMM_M {
+    ldfp ufp0, seg, sib, "DISPLACEMENT", dataSize=8
+    ldfp ufp1, seg, sib, "DISPLACEMENT + 8", dataSize=8
+    vaddf dest=xmm0, src1=xmm0v, src2=ufp0, size=4, VL=16
+    vclear dest=xmm2, destVL=16
+};
+def macroop VADDPS_XMM_XMM_XMM_P {
+    ldfp ufp0, seg, sib, "DISPLACEMENT", dataSize=8
+    ldfp ufp1, seg, sib, "DISPLACEMENT + 8", dataSize=8
+    vaddf dest=xmm0, src1=xmm0v, src2=ufp0, size=4, VL=16
+    vclear dest=xmm2, destVL=16
+};
 def macroop VADDPS_YMM_YMM {
     vaddf dest=xmm0, src1=xmm0v, src2=xmmrm, size=4, VL=32
 };
-# def macroop VADDPS_YMM_YMM_XMM_XMM {
-#     vaddf dest=xmm0, src1=xmm0v, src2=xmmrm, size=4, VL=32
-# };
+def macroop VADDPS_YMM_YMM_XMM_XMM {
+    vaddf dest=xmm0, src1=xmm0v, src2=xmmrm, size=4, VL=32
+};
 def macroop VADDPS_YMM_M {
     ldfp ufp0, seg, sib, "DISPLACEMENT", dataSize=8
     ldfp ufp1, seg, sib, "DISPLACEMENT + 8", dataSize=8
@@ -65,18 +65,18 @@ def macroop VADDPS_YMM_YMM_P {
     ldfp ufp3, seg, sib, "DISPLACEMENT + 24", dataSize=8
     vaddf dest=xmm0, src1=xmm0v, src2=ufp0, size=4, VL=32
 };
-# def macroop VADDPS_YMM_YMM_XMM_M {
-#     ldfp ufp0, seg, sib, "DISPLACEMENT", dataSize=8
-#     ldfp ufp1, seg, sib, "DISPLACEMENT + 8", dataSize=8
-#     ldfp ufp2, seg, sib, "DISPLACEMENT + 16", dataSize=8
-#     ldfp ufp3, seg, sib, "DISPLACEMENT + 24", dataSize=8
-#     vaddf dest=xmm0, src1=xmm0v, src2=ufp0, size=4, VL=32
-# };
-# def macroop VADDPS_YMM_YMM_XMM_P {
-#     ldfp ufp0, seg, sib, "DISPLACEMENT", dataSize=8
-#     ldfp ufp1, seg, sib, "DISPLACEMENT + 8", dataSize=8
-#     ldfp ufp2, seg, sib, "DISPLACEMENT + 16", dataSize=8
-#     ldfp ufp3, seg, sib, "DISPLACEMENT + 24", dataSize=8
-#     vaddf dest=xmm0, src1=xmm0v, src2=ufp0, size=4, VL=32
-# };
+def macroop VADDPS_YMM_YMM_XMM_M {
+    ldfp ufp0, seg, sib, "DISPLACEMENT", dataSize=8
+    ldfp ufp1, seg, sib, "DISPLACEMENT + 8", dataSize=8
+    ldfp ufp2, seg, sib, "DISPLACEMENT + 16", dataSize=8
+    ldfp ufp3, seg, sib, "DISPLACEMENT + 24", dataSize=8
+    vaddf dest=xmm0, src1=xmm0v, src2=ufp0, size=4, VL=32
+};
+def macroop VADDPS_YMM_YMM_XMM_P {
+    ldfp ufp0, seg, sib, "DISPLACEMENT", dataSize=8
+    ldfp ufp1, seg, sib, "DISPLACEMENT + 8", dataSize=8
+    ldfp ufp2, seg, sib, "DISPLACEMENT + 16", dataSize=8
+    ldfp ufp3, seg, sib, "DISPLACEMENT + 24", dataSize=8
+    vaddf dest=xmm0, src1=xmm0v, src2=ufp0, size=4, VL=32
+};
 '''
