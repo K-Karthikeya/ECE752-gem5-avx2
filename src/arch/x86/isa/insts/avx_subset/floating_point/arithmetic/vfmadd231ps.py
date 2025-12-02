96b2e2f99f008305191531a282017df887552fee
Karthikeya Kotha <kkotha2@wisc.edu>
2025-12-01 23:10:09 -0600

    FMA tests-new macroops

--- src/arch/x86/isa/insts/avx_subset/floating_point/arithmetic/vfmadd231ps.py
+++ src/arch/x86/isa/insts/avx_subset/floating_point/arithmetic/vfmadd231ps.py
@@ -1,70 +1,37 @@
 microcode = '''
-# FMA 231 form: dest = (src2 * src1) + dest (accumulator). Implemented via fused microop vfmadd231f.
-def macroop VFMADD231PS_XMM_XMM {
+# FMA 231: dest = (src2 * src1) + dest. Use fused microop vfmadd231f.
+
+# 128-bit width (VEX.L=0); reg-reg and reg/mem forms
+def macroop VFMADD231PS_128_XMM_XMM {
     vfmadd231f dest=xmm0, src1=xmm0v, src2=xmmrm, size=4, VL=16
     vclear dest=xmm2, destVL=16
 };
-def macroop VFMADD231PS_XMM_XMM_XMM_XMM {
-    vfmadd231f dest=xmm0, src1=xmm0v, src2=xmmrm, size=4, VL=16
-    vclear dest=xmm2, destVL=16
-};
-def macroop VFMADD231PS_XMM_M {
-    ldfp128 ufp0, seg, sib, "DISPLACEMENT", dataSize=8
+def macroop VFMADD231PS_128_XMM_M {
+    ldfp128 ufp0, seg, sib, "DISPLACEMENT", dataSize=16
     vfmadd231f dest=xmm0, src1=xmm0v, src2=ufp0, size=4, VL=16
     vclear dest=xmm2, destVL=16
 };
-def macroop VFMADD231PS_XMM_XMM_M {
-    ldfp128 ufp0, seg, sib, "DISPLACEMENT", dataSize=8
-    vfmadd231f dest=xmm0, src1=xmm0v, src2=ufp0, size=4, VL=16
-    vclear dest=xmm2, destVL=16
-};
-def macroop VFMADD231PS_XMM_XMM_P {
-    ldfp128 ufp0, seg, sib, "DISPLACEMENT", dataSize=8
-    vfmadd231f dest=xmm0, src1=xmm0v, src2=ufp0, size=4, VL=16
-    vclear dest=xmm2, destVL=16
-};
-def macroop VFMADD231PS_XMM_XMM_XMM_M {
-    ldfp128 ufp0, seg, sib, "DISPLACEMENT", dataSize=8
-    vfmadd231f dest=xmm0, src1=xmm0v, src2=ufp0, size=4, VL=16
-    vclear dest=xmm2, destVL=16
-};
-def macroop VFMADD231PS_XMM_XMM_XMM_P {
-    ldfp128 ufp0, seg, sib, "DISPLACEMENT", dataSize=8
+def macroop VFMADD231PS_128_XMM_P {
+    rdip t7
+    ldfp128 ufp0, seg, riprel, "DISPLACEMENT", dataSize=16
     vfmadd231f dest=xmm0, src1=xmm0v, src2=ufp0, size=4, VL=16
     vclear dest=xmm2, destVL=16
 };
 
-def macroop VFMADD231PS_YMM_YMM {
-    limm t0, 0
-    limm t1, 0
-    limm t2, 0
-    # Debug: mark register variant selection
-    vclear dest=xmm2, destVL=0
+# 256-bit width (VEX.L=1); reg-reg and reg/mem forms
+def macroop VFMADD231PS_256_XMM_XMM {
     vfmadd231f dest=xmm0, src1=xmm0v, src2=xmmrm, size=4, VL=32
+    vclear dest=xmm4, destVL=32
 };
-def macroop VFMADD231PS_YMM_YMM_XMM_XMM {
-    vfmadd231f dest=xmm0, src1=xmm0v, src2=xmmrm, size=4, VL=32
-};
-def macroop VFMADD231PS_YMM_M {
-    ldfp256 ufp0, seg, sib, "DISPLACEMENT", dataSize=8
-    # Debug: mark memory variant selection
-    vclear dest=xmm2, destVL=0
+def macroop VFMADD231PS_256_XMM_M {
+    ldfp256 ufp0, seg, sib, "DISPLACEMENT", dataSize=32
     vfmadd231f dest=xmm0, src1=xmm0v, src2=ufp0, size=4, VL=32
+    vclear dest=xmm4, destVL=32
 };
-def macroop VFMADD231PS_YMM_YMM_M {
-    ldfp256 ufp0, seg, sib, "DISPLACEMENT", dataSize=8
+def macroop VFMADD231PS_256_XMM_P {
+    rdip t7
+    ldfp256 ufp0, seg, riprel, "DISPLACEMENT", dataSize=32
     vfmadd231f dest=xmm0, src1=xmm0v, src2=ufp0, size=4, VL=32
-};
-def macroop VFMADD231PS_YMM_YMM_P {
-    ldfp256 ufp0, seg, sib, "DISPLACEMENT", dataSize=8
-    vfmadd231f dest=xmm0, src1=xmm0v, src2=ufp0, size=4, VL=32
-};
-def macroop VFMADD231PS_YMM_YMM_XMM_M {
-    ldfp256 ufp0, seg, sib, "DISPLACEMENT", dataSize=8
-    vfmadd231f dest=xmm0, src1=xmm0v, src2=ufp0, size=4, VL=32
-};
-def macroop VFMADD231PS_YMM_YMM_XMM_P {
-    ldfp256 ufp0, seg, sib, "DISPLACEMENT", dataSize=8
-    vfmadd231f dest=xmm0, src1=xmm0v, src2=ufp0, size=4, VL=32
+    vclear dest=xmm4, destVL=32
 };
 '''
