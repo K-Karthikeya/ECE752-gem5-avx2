8754069755eba378c01e43188d09d4a8a57ca8f8
Joshua Noronha <jnoronha@wisc.edu>
2025-12-03 11:39:47 -0600

    Minor fix

--- src/arch/x86/isa/insts/avx_subset/double/data_transfer/vmovddup.py
+++ src/arch/x86/isa/insts/avx_subset/double/data_transfer/vmovddup.py
@@ -14,11 +14,18 @@ def macroop VMOVDDUP_XMM_XMM {
     vdupf dest=xmm0, src=xmmrm, lane=0, size=8, VL=16
     vclear dest=xmm2, destVL=16
 };
+def macroop VMOVDDUP_XMM_XMM_XMM_XMM {
+    vdupf dest=xmm0, src=xmmrm, lane=0, size=8, VL=16
+    vclear dest=xmm2, destVL=16
+};
 def macroop VMOVDDUP_YMM_M {
     ldfp256 ufp0, seg, sib, "DISPLACEMENT", dataSize=8
     vdupf_half dest=xmm0, src=ufp0, size=8, VL=32
 };
 def macroop VMOVDDUP_YMM_YMM {
+    vdupf_half dest=xmm0, src=xmmrm, size=8, VL=32
+};
+def macroop VMOVDDUP_YMM_YMM_XMM_XMM {
     vdupf_half dest=xmm0, src=xmmrm, size=8, VL=32
 };
 
