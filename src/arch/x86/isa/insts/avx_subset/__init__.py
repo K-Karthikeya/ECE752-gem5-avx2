c0b91cc398162b41845d1f5e64a26445d4c002e6
Joshua Noronha <jnoronha@wisc.edu>
2025-12-03 12:11:04 -0600

    Completed Doubles

--- src/arch/x86/isa/insts/avx_subset/__init__.py
+++ src/arch/x86/isa/insts/avx_subset/__init__.py
@@ -4,6 +4,7 @@
 from .floating_point.arithmetic import vaddps, vmulps, vxorps
 from .floating_point.data_transfer import vmovups, vmovaps, vzeroupper
 from .double.arithmetic import vaddpd, vsubpd, vmulpd, vdivpd
+from .double.data_transfer import vbroadcastsd, vmovddup
 
 microcode = ""
 microcode += vaddps.microcode
@@ -16,3 +17,5 @@ microcode += vaddpd.microcode
 microcode += vsubpd.microcode
 microcode += vmulpd.microcode
 microcode += vdivpd.microcode
+microcode += vbroadcastsd.microcode
+microcode += vmovddup.microcode
--- src/arch/x86/isa/insts/avx_subset/double/data_transfer/vmovddup.py
+++ src/arch/x86/isa/insts/avx_subset/double/data_transfer/vmovddup.py
@@ -4,7 +4,18 @@ def macroop VMOVDDUP_XMM_M {
     vdupf dest=xmm0, src=ufp0, lane=0, size=8, VL=16
     vclear dest=xmm2, destVL=16
 };
+def macroop VMOVDDUP_XMM_M_M {
+    ldfp128 ufp0, seg, sib, "DISPLACEMENT", dataSize=8
+    vdupf dest=xmm0, src=ufp0, lane=0, size=8, VL=16
+    vclear dest=xmm2, destVL=16
+};
 def macroop VMOVDDUP_XMM_P {
+    rdip t7
+    ldfp128 ufp0, seg, riprel, "DISPLACEMENT", dataSize=8
+    vdupf dest=xmm0, src=ufp0, lane=0, size=8, VL=16
+    vclear dest=xmm2, destVL=16
+};
+def macroop VMOVDDUP_XMM_M_P {
     rdip t7
     ldfp128 ufp0, seg, riprel, "DISPLACEMENT", dataSize=8
     vdupf dest=xmm0, src=ufp0, lane=0, size=8, VL=16
@@ -18,14 +29,40 @@ def macroop VMOVDDUP_XMM_XMM_XMM_XMM {
     vdupf dest=xmm0, src=xmmrm, lane=0, size=8, VL=16
     vclear dest=xmm2, destVL=16
 };
+def macroop VMOVDDUP_XMM_XMM_XMM_M {
+    ldfp128 ufp0, seg, sib, "DISPLACEMENT", dataSize=8
+    vdupf dest=xmm0, src=ufp0, lane=0, size=8, VL=16
+    vclear dest=xmm2, destVL=16
+};
+def macroop VMOVDDUP_XMM_XMM_XMM_P {
+    rdip t7
+    ldfp128 ufp0, seg, riprel, "DISPLACEMENT", dataSize=8
+    vdupf dest=xmm0, src=ufp0, lane=0, size=8, VL=16
+    vclear dest=xmm2, destVL=16
+};
 def macroop VMOVDDUP_YMM_M {
     ldfp256 ufp0, seg, sib, "DISPLACEMENT", dataSize=8
+    vdupf_half dest=xmm0, src=ufp0, size=8, VL=32
+};
+def macroop VMOVDDUP_YMM_M_M {
+    ldfp256 ufp0, seg, sib, "DISPLACEMENT", dataSize=8
+    vdupf_half dest=xmm0, src=ufp0, size=8, VL=32
+};
+def macroop VMOVDDUP_YMM_M_P {
+    rdip t7
+    ldfp256 ufp0, seg, riprel, "DISPLACEMENT", dataSize=8
     vdupf_half dest=xmm0, src=ufp0, size=8, VL=32
 };
 def macroop VMOVDDUP_YMM_YMM {
     vdupf_half dest=xmm0, src=xmmrm, size=8, VL=32
 };
 def macroop VMOVDDUP_YMM_YMM_XMM_XMM {
+    vdupf_half dest=xmm0, src=xmmrm, size=8, VL=32
+};
+def macroop VMOVDDUP_YMM_YMM_XMM_M {
+    vdupf_half dest=xmm0, src=xmmrm, size=8, VL=32
+};
+def macroop VMOVDDUP_YMM_YMM_XMM_P {
     vdupf_half dest=xmm0, src=xmmrm, size=8, VL=32
 };
 
