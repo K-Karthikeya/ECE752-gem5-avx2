8f04f1ccb9d588edb860448acdce542a1b24ac31
Karthikeya Kotha <kkotha2@wisc.edu>
2025-12-03 19:43:22 -0600

    Comprehensive Testing-Fixing FMA#5

--- src/arch/x86/isa/insts/avx_subset/floating_point/arithmetic/vfmadd231ps.py
+++ src/arch/x86/isa/insts/avx_subset/floating_point/arithmetic/vfmadd231ps.py
@@ -5,35 +5,35 @@ microcode = '''
 
 # 128-bit width (VEX.L=0); reg-reg and reg/mem forms
 def macroop VFMADD231PS_128_XMM_XMM {
-    vfmadd231f dest=xmm0, src1=xmmrm, src2=xmm0v, size=4, VL=16
+    vfmadd231f dest=xmm0, src1=xmm0v, src2=xmmrm, size=4, VL=16
     vclear dest=xmm2, destVL=16
 };
 def macroop VFMADD231PS_128_XMM_M {
     ldfp128 ufp0, seg, sib, "DISPLACEMENT", dataSize=8
-    vfmadd231f dest=xmm0, src1=ufp0, src2=xmm0v, size=4, VL=16
+    vfmadd231f dest=xmm0, src1=xmm0v, src2=ufp0, size=4, VL=16
     vclear dest=xmm2, destVL=16
 };
 def macroop VFMADD231PS_128_XMM_P {
     rdip t7
     ldfp128 ufp0, seg, riprel, "DISPLACEMENT", dataSize=8
-    vfmadd231f dest=xmm0, src1=ufp0, src2=xmm0v, size=4, VL=16
+    vfmadd231f dest=xmm0, src1=xmm0v, src2=ufp0, size=4, VL=16
     vclear dest=xmm2, destVL=16
 };
 
 # 256-bit width (VEX.L=1); reg-reg and reg/mem forms
 def macroop VFMADD231PS_256_XMM_XMM {
-    vfmadd231f dest=xmm0, src1=xmmrm, src2=xmm0v, size=4, VL=32
+    vfmadd231f dest=xmm0, src1=xmm0v, src2=xmmrm, size=4, VL=32
     vclear dest=xmm4, destVL=32
 };
 def macroop VFMADD231PS_256_XMM_M {
     ldfp256 ufp0, seg, sib, "DISPLACEMENT", dataSize=8
-    vfmadd231f dest=xmm0, src1=ufp0, src2=xmm0v, size=4, VL=32
+    vfmadd231f dest=xmm0, src1=xmm0v, src2=ufp0, size=4, VL=32
     vclear dest=xmm4, destVL=32
 };
 def macroop VFMADD231PS_256_XMM_P {
     rdip t7
     ldfp256 ufp0, seg, riprel, "DISPLACEMENT", dataSize=8
-    vfmadd231f dest=xmm0, src1=ufp0, src2=xmm0v, size=4, VL=32
+    vfmadd231f dest=xmm0, src1=xmm0v, src2=ufp0, size=4, VL=32
     vclear dest=xmm4, destVL=32
 };
 '''
