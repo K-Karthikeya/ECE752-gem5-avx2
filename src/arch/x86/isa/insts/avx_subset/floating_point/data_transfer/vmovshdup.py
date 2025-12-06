8ce0bd2483106e5087f83db0b4d88f84c14b07bb
kapish <singhkapish27@gmail.com>
2025-12-06 17:56:02 -0600

    adding other insts - error resolving 2

--- src/arch/x86/isa/insts/avx_subset/floating_point/data_transfer/vmovshdup.py
+++ src/arch/x86/isa/insts/avx_subset/floating_point/data_transfer/vmovshdup.py
@@ -8,6 +8,16 @@ def macroop VMOVSHDUP_XMM_XMM_XMM_XMM {
     vmovshdup dest=xmm0, src1=xmmrm, size=4, VL=16
     vclear dest=xmm2, destVL=16
 };
+def macroop VMOVSHDUP_XMM_XMM_XMM_M {
+    ldfp128 ufp0, seg, sib, "DISPLACEMENT", dataSize=8
+    vmovshdup dest=xmm0, src1=ufp0, size=4, VL=16
+    vclear dest=xmm2, destVL=16
+};
+def macroop VMOVSHDUP_XMM_XMM_XMM_P {
+    ldfp128 ufp0, seg, riprel, "DISPLACEMENT", dataSize=8
+    vmovshdup dest=xmm0, src1=ufp0, size=4, VL=16
+    vclear dest=xmm2, destVL=16
+};
 def macroop VMOVSHDUP_XMM_M {
     ldfp128 ufp0, seg, sib, "DISPLACEMENT", dataSize=8
     vmovshdup dest=xmm0, src1=ufp0, size=4, VL=16
@@ -24,6 +34,14 @@ def macroop VMOVSHDUP_YMM_YMM {
 };
 def macroop VMOVSHDUP_YMM_YMM_XMM_XMM {
     vmovshdup dest=xmm0, src1=xmmrm, size=4, VL=32
+};
+def macroop VMOVSHDUP_YMM_YMM_XMM_M {
+    ldfp256 ufp0, seg, sib, "DISPLACEMENT", dataSize=8
+    vmovshdup dest=xmm0, src1=ufp0, size=4, VL=32
+};
+def macroop VMOVSHDUP_YMM_YMM_XMM_P {
+    ldfp256 ufp0, seg, riprel, "DISPLACEMENT", dataSize=8
+    vmovshdup dest=xmm0, src1=ufp0, size=4, VL=32
 };
 def macroop VMOVSHDUP_YMM_M {
     ldfp256 ufp0, seg, sib, "DISPLACEMENT", dataSize=8
--- src/arch/x86/isa/insts/avx_subset/floating_point/data_transfer/vmovsldup.py
+++ src/arch/x86/isa/insts/avx_subset/floating_point/data_transfer/vmovsldup.py
@@ -8,6 +8,16 @@ def macroop VMOVSLDUP_XMM_XMM_XMM_XMM {
     vmovsldup dest=xmm0, src1=xmmrm, size=4, VL=16
     vclear dest=xmm2, destVL=16
 };
+def macroop VMOVSLDUP_XMM_XMM_XMM_M {
+    ldfp128 ufp0, seg, sib, "DISPLACEMENT", dataSize=8
+    vmovsldup dest=xmm0, src1=ufp0, size=4, VL=16
+    vclear dest=xmm2, destVL=16
+};
+def macroop VMOVSLDUP_XMM_XMM_XMM_P {
+    ldfp128 ufp0, seg, riprel, "DISPLACEMENT", dataSize=8
+    vmovsldup dest=xmm0, src1=ufp0, size=4, VL=16
+    vclear dest=xmm2, destVL=16
+};
 def macroop VMOVSLDUP_XMM_M {
     ldfp128 ufp0, seg, sib, "DISPLACEMENT", dataSize=8
     vmovsldup dest=xmm0, src1=ufp0, size=4, VL=16
@@ -24,6 +34,14 @@ def macroop VMOVSLDUP_YMM_YMM {
 };
 def macroop VMOVSLDUP_YMM_YMM_XMM_XMM {
     vmovsldup dest=xmm0, src1=xmmrm, size=4, VL=32
+};
+def macroop VMOVSLDUP_YMM_YMM_XMM_M {
+    ldfp256 ufp0, seg, sib, "DISPLACEMENT", dataSize=8
+    vmovsldup dest=xmm0, src1=ufp0, size=4, VL=32
+};
+def macroop VMOVSLDUP_YMM_YMM_XMM_P {
+    ldfp256 ufp0, seg, riprel, "DISPLACEMENT", dataSize=8
+    vmovsldup dest=xmm0, src1=ufp0, size=4, VL=32
 };
 def macroop VMOVSLDUP_YMM_M {
     ldfp256 ufp0, seg, sib, "DISPLACEMENT", dataSize=8
