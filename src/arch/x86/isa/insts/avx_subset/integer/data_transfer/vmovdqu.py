b31b79737e31a88bd92e52f52915057129c54c79
Karthikeya Kotha <kkotha2@wisc.edu>
2025-12-07 19:13:17 -0600

    Integer Instrctions Build error#5

--- src/arch/x86/isa/insts/avx_subset/integer/data_transfer/vmovdqu.py
+++ src/arch/x86/isa/insts/avx_subset/integer/data_transfer/vmovdqu.py
@@ -1,44 +1,36 @@
 microcode = '''
 # VMOVDQU: unaligned move of packed dword/qword integers
-# Same pattern as VMOVUPS (floating-point unaligned)
+# Pattern: load directly into xmm0, store directly from xmm0
 
 # 128-bit width (VEX.L=0); load and store forms
 def macroop VMOVDQU_128_M {
-    ldfp128 ufp0, seg, sib, "DISPLACEMENT", dataSize=8
-    movfp dest=xmm0, src1=ufp0, size=16, VL=16
+    ldfp128 xmm0, seg, sib, "DISPLACEMENT", dataSize=8
 };
 def macroop VMOVDQU_128_P {
     rdip t7
-    ldfp128 ufp0, seg, riprel, "DISPLACEMENT", dataSize=8
-    movfp dest=xmm0, src1=ufp0, size=16, VL=16
+    ldfp128 xmm0, seg, riprel, "DISPLACEMENT", dataSize=8
 };
 def macroop VMOVDQU_M_128_M {
-    ldfp128 ufp0, seg, sib, "DISPLACEMENT", dataSize=8
-    stfp128 ufp0, seg, sib, "DISPLACEMENT", dataSize=8
+    stfp128 xmm0, seg, sib, "DISPLACEMENT", dataSize=8
 };
 def macroop VMOVDQU_M_128_P {
     rdip t7
-    ldfp128 ufp0, seg, riprel, "DISPLACEMENT", dataSize=8
-    stfp128 ufp0, seg, sib, "DISPLACEMENT", dataSize=8
+    stfp128 xmm0, seg, riprel, "DISPLACEMENT", dataSize=8
 };
 
 # 256-bit width (VEX.L=1); load and store forms
 def macroop VMOVDQU_256_M {
-    ldfp256 ufp0, seg, sib, "DISPLACEMENT", dataSize=8
-    movfp dest=xmm0, src1=ufp0, size=32, VL=32
+    ldfp256 xmm0, seg, sib, "DISPLACEMENT", dataSize=8
 };
 def macroop VMOVDQU_256_P {
     rdip t7
-    ldfp256 ufp0, seg, riprel, "DISPLACEMENT", dataSize=8
-    movfp dest=xmm0, src1=ufp0, size=32, VL=32
+    ldfp256 xmm0, seg, riprel, "DISPLACEMENT", dataSize=8
 };
 def macroop VMOVDQU_M_256_M {
-    ldfp256 ufp0, seg, sib, "DISPLACEMENT", dataSize=8
-    stfp256 ufp0, seg, sib, "DISPLACEMENT", dataSize=8
+    stfp256 xmm0, seg, sib, "DISPLACEMENT", dataSize=8
 };
 def macroop VMOVDQU_M_256_P {
     rdip t7
-    ldfp256 ufp0, seg, riprel, "DISPLACEMENT", dataSize=8
-    stfp256 ufp0, seg, sib, "DISPLACEMENT", dataSize=8
+    stfp256 xmm0, seg, riprel, "DISPLACEMENT", dataSize=8
 };
 '''
