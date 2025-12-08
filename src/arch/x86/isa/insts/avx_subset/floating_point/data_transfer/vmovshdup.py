258f18292539c79f0885c11a598cf53809b6f15b
kapish <singhkapish27@gmail.com>
2025-12-08 01:37:21 -0600

    adding instructions with updated nomenclature and testing -1

--- src/arch/x86/isa/insts/avx_subset/floating_point/data_transfer/vmovshdup.py
+++ src/arch/x86/isa/insts/avx_subset/floating_point/data_transfer/vmovshdup.py
@@ -1,21 +1,8 @@
 microcode = '''
 # Duplicate high 32-bit float in each 64-bit lane
+# 128-bit (VEX.L=0)
 def macroop VMOVSHDUP_128_XMM_XMM {
     vmovshdup dest=xmm0, src1=xmmrm, size=4, VL=16
-    vclear dest=xmm2, destVL=16
-};
-def macroop VMOVSHDUP_128_XMM_XMM_XMM_XMM {
-    vmovshdup dest=xmm0, src1=xmmrm, size=4, VL=16
-    vclear dest=xmm2, destVL=16
-};
-def macroop VMOVSHDUP_128_XMM_XMM_XMM_M {
-    ldfp128 ufp0, seg, sib, "DISPLACEMENT", dataSize=8
-    vmovshdup dest=xmm0, src1=ufp0, size=4, VL=16
-    vclear dest=xmm2, destVL=16
-};
-def macroop VMOVSHDUP_128_XMM_XMM_XMM_P {
-    ldfp128 ufp0, seg, riprel, "DISPLACEMENT", dataSize=8
-    vmovshdup dest=xmm0, src1=ufp0, size=4, VL=16
     vclear dest=xmm2, destVL=16
 };
 def macroop VMOVSHDUP_128_XMM_M {
@@ -29,25 +16,15 @@ def macroop VMOVSHDUP_128_XMM_P {
     vclear dest=xmm2, destVL=16
 };
 
-def macroop VMOVSHDUP_256_YMM_YMM {
+# 256-bit (VEX.L=1)
+def macroop VMOVSHDUP_256_XMM_XMM {
     vmovshdup dest=xmm0, src1=xmmrm, size=4, VL=32
 };
-def macroop VMOVSHDUP_256_YMM_YMM_XMM_XMM {
-    vmovshdup dest=xmm0, src1=xmmrm, size=4, VL=32
-};
-def macroop VMOVSHDUP_256_YMM_YMM_XMM_M {
+def macroop VMOVSHDUP_256_XMM_M {
     ldfp256 ufp0, seg, sib, "DISPLACEMENT", dataSize=8
     vmovshdup dest=xmm0, src1=ufp0, size=4, VL=32
 };
-def macroop VMOVSHDUP_256_YMM_YMM_XMM_P {
-    ldfp256 ufp0, seg, riprel, "DISPLACEMENT", dataSize=8
-    vmovshdup dest=xmm0, src1=ufp0, size=4, VL=32
-};
-def macroop VMOVSHDUP_256_YMM_M {
-    ldfp256 ufp0, seg, sib, "DISPLACEMENT", dataSize=8
-    vmovshdup dest=xmm0, src1=ufp0, size=4, VL=32
-};
-def macroop VMOVSHDUP_256_YMM_P {
+def macroop VMOVSHDUP_256_XMM_P {
     ldfp256 ufp0, seg, riprel, "DISPLACEMENT", dataSize=8
     vmovshdup dest=xmm0, src1=ufp0, size=4, VL=32
 };
--- src/arch/x86/isa/insts/avx_subset/floating_point/data_transfer/vmovsldup.py
+++ src/arch/x86/isa/insts/avx_subset/floating_point/data_transfer/vmovsldup.py
@@ -1,21 +1,8 @@
 microcode = '''
 # Duplicate low 32-bit float in each 64-bit lane
+# 128-bit (VEX.L=0)
 def macroop VMOVSLDUP_128_XMM_XMM {
     vmovsldup dest=xmm0, src1=xmmrm, size=4, VL=16
-    vclear dest=xmm2, destVL=16
-};
-def macroop VMOVSLDUP_128_XMM_XMM_XMM_XMM {
-    vmovsldup dest=xmm0, src1=xmmrm, size=4, VL=16
-    vclear dest=xmm2, destVL=16
-};
-def macroop VMOVSLDUP_128_XMM_XMM_XMM_M {
-    ldfp128 ufp0, seg, sib, "DISPLACEMENT", dataSize=8
-    vmovsldup dest=xmm0, src1=ufp0, size=4, VL=16
-    vclear dest=xmm2, destVL=16
-};
-def macroop VMOVSLDUP_128_XMM_XMM_XMM_P {
-    ldfp128 ufp0, seg, riprel, "DISPLACEMENT", dataSize=8
-    vmovsldup dest=xmm0, src1=ufp0, size=4, VL=16
     vclear dest=xmm2, destVL=16
 };
 def macroop VMOVSLDUP_128_XMM_M {
@@ -29,25 +16,15 @@ def macroop VMOVSLDUP_128_XMM_P {
     vclear dest=xmm2, destVL=16
 };
 
-def macroop VMOVSLDUP_256_YMM_YMM {
+# 256-bit (VEX.L=1)
+def macroop VMOVSLDUP_256_XMM_XMM {
     vmovsldup dest=xmm0, src1=xmmrm, size=4, VL=32
 };
-def macroop VMOVSLDUP_256_YMM_YMM_XMM_XMM {
-    vmovsldup dest=xmm0, src1=xmmrm, size=4, VL=32
-};
-def macroop VMOVSLDUP_256_YMM_YMM_XMM_M {
+def macroop VMOVSLDUP_256_XMM_M {
     ldfp256 ufp0, seg, sib, "DISPLACEMENT", dataSize=8
     vmovsldup dest=xmm0, src1=ufp0, size=4, VL=32
 };
-def macroop VMOVSLDUP_256_YMM_YMM_XMM_P {
-    ldfp256 ufp0, seg, riprel, "DISPLACEMENT", dataSize=8
-    vmovsldup dest=xmm0, src1=ufp0, size=4, VL=32
-};
-def macroop VMOVSLDUP_256_YMM_M {
-    ldfp256 ufp0, seg, sib, "DISPLACEMENT", dataSize=8
-    vmovsldup dest=xmm0, src1=ufp0, size=4, VL=32
-};
-def macroop VMOVSLDUP_256_YMM_P {
+def macroop VMOVSLDUP_256_XMM_P {
     ldfp256 ufp0, seg, riprel, "DISPLACEMENT", dataSize=8
     vmovsldup dest=xmm0, src1=ufp0, size=4, VL=32
 };
