507d1984d4065fe39fc76aabbd089e4fc8853f0d
Joshua Noronha <jnoronha@wisc.edu>
2025-12-07 18:56:38 -0600

    Updated naming convention

--- src/arch/x86/isa/insts/avx_subset/double/arithmetic/vaddpd.py
+++ src/arch/x86/isa/insts/avx_subset/double/arithmetic/vaddpd.py
@@ -1,4 +1,32 @@
 microcode = '''
+def macroop VADDPD_128_XMM_XMM {
+    vaddf dest=xmm0, src1=xmm0v, src2=xmmrm, size=8, VL=16
+    vclear dest=xmm2, destVL=16
+};
+def macroop VADDPD_128_XMM_M {
+    ldfp128 ufp0, seg, sib, "DISPLACEMENT", dataSize=8
+    vaddf dest=xmm0, src1=xmm0v, src2=ufp0, size=8, VL=16
+    vclear dest=xmm2, destVL=16
+};
+def macroop VADDPD_128_XMM_P {
+    rdip t7
+    ldfp128 ufp0, seg, riprel, "DISPLACEMENT", dataSize=8
+    vaddf dest=xmm0, src1=xmm0v, src2=ufp0, size=8, VL=16
+    vclear dest=xmm2, destVL=16
+};
+
+def macroop VADDPD_256_XMM_XMM {
+    vaddf dest=xmm0, src1=xmm0v, src2=xmmrm, size=8, VL=32
+};
+def macroop VADDPD_256_XMM_M {
+    ldfp256 ufp0, seg, sib, "DISPLACEMENT", dataSize=8
+    vaddf dest=xmm0, src1=xmm0v, src2=ufp0, size=8, VL=32
+};
+def macroop VADDPD_256_XMM_P {
+    rdip t7
+    ldfp256 ufp0, seg, riprel, "DISPLACEMENT", dataSize=8
+    vaddf dest=xmm0, src1=xmm0v, src2=ufp0, size=8, VL=32
+};
 def macroop VADDPD_XMM_XMM {
     # src2 provided directly via ModRM.rm (xmmrm) avoiding env.regm usage.
     vaddf dest=xmm0, src1=xmm0v, src2=xmmrm, size=8, VL=16
--- src/arch/x86/isa/insts/avx_subset/double/arithmetic/vdivpd.py
+++ src/arch/x86/isa/insts/avx_subset/double/arithmetic/vdivpd.py
@@ -1,4 +1,32 @@
 microcode = '''
+def macroop VDIVPD_128_XMM_XMM {
+    vdivf dest=xmm0, src1=xmm0v, src2=xmmrm, size=8, VL=16
+    vclear dest=xmm2, destVL=16
+};
+def macroop VDIVPD_128_XMM_M {
+    ldfp128 ufp0, seg, sib, "DISPLACEMENT", dataSize=8
+    vdivf dest=xmm0, src1=xmm0v, src2=ufp0, size=8, VL=16
+    vclear dest=xmm2, destVL=16
+};
+def macroop VDIVPD_128_XMM_P {
+    rdip t7
+    ldfp128 ufp0, seg, riprel, "DISPLACEMENT", dataSize=8
+    vdivf dest=xmm0, src1=xmm0v, src2=ufp0, size=8, VL=16
+    vclear dest=xmm2, destVL=16
+};
+
+def macroop VDIVPD_256_XMM_XMM {
+    vdivf dest=xmm0, src1=xmm0v, src2=xmmrm, size=8, VL=32
+};
+def macroop VDIVPD_256_XMM_M {
+    ldfp256 ufp0, seg, sib, "DISPLACEMENT", dataSize=8
+    vdivf dest=xmm0, src1=xmm0v, src2=ufp0, size=8, VL=32
+};
+def macroop VDIVPD_256_XMM_P {
+    rdip t7
+    ldfp256 ufp0, seg, riprel, "DISPLACEMENT", dataSize=8
+    vdivf dest=xmm0, src1=xmm0v, src2=ufp0, size=8, VL=32
+};
 def macroop VMULPD_XMM_XMM {
     vdivf dest=xmm0, src1=xmm0v, src2=xmmrm, size=8, VL=16
     vclear dest=xmm2, destVL=16
