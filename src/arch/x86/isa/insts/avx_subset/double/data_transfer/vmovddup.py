e2b64008740b609575954878e4dfb75235b5e5b9
Karthikeya Kotha <kkotha2@wisc.edu>
2025-12-08 17:59:40 -0600

    One last attempt to fix dups and broadcasts-2

--- src/arch/x86/isa/insts/avx_subset/double/data_transfer/vmovddup.py
+++ src/arch/x86/isa/insts/avx_subset/double/data_transfer/vmovddup.py
@@ -18,12 +18,12 @@ def macroop VMOVDDUP_256_XMM_XMM {
     vdupf_half dest=xmm0, src=xmmrm, size=8, VL=32
 };
 def macroop VMOVDDUP_256_XMM_M {
-    ldfp256 ufp0, seg, sib, "DISPLACEMENT", dataSize=32
+    ldfp256 ufp0, seg, sib, "DISPLACEMENT", dataSize=8
     vdupf_half dest=xmm0, src=ufp0, size=8, VL=32
 };
 def macroop VMOVDDUP_256_XMM_P {
     rdip t7
-    ldfp256 ufp0, seg, riprel, "DISPLACEMENT", dataSize=32
+    ldfp256 ufp0, seg, riprel, "DISPLACEMENT", dataSize=8
     vdupf_half dest=xmm0, src=ufp0, size=8, VL=32
 };
 """
