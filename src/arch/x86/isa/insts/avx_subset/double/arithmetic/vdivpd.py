bdb9b34d5d4ec72474ce8c79a475177e8a5ad46d
Joshua Noronha <jnoronha@wisc.edu>
2025-12-07 19:31:13 -0600

    Reworking decoder

--- src/arch/x86/isa/insts/avx_subset/double/arithmetic/vdivpd.py
+++ src/arch/x86/isa/insts/avx_subset/double/arithmetic/vdivpd.py
@@ -14,7 +14,6 @@ def macroop VDIVPD_128_XMM_P {
     vdivf dest=xmm0, src1=xmm0v, src2=ufp0, size=8, VL=16
     vclear dest=xmm2, destVL=16
 };
-
 def macroop VDIVPD_256_XMM_XMM {
     vdivf dest=xmm0, src1=xmm0v, src2=xmmrm, size=8, VL=32
 };
@@ -25,65 +24,6 @@ def macroop VDIVPD_256_XMM_M {
 def macroop VDIVPD_256_XMM_P {
     rdip t7
     ldfp256 ufp0, seg, riprel, "DISPLACEMENT", dataSize=8
-    vdivf dest=xmm0, src1=xmm0v, src2=ufp0, size=8, VL=32
-};
-def macroop VMULPD_XMM_XMM {
-    vdivf dest=xmm0, src1=xmm0v, src2=xmmrm, size=8, VL=16
-    vclear dest=xmm2, destVL=16
-};
-def macroop VMULPD_XMM_XMM_XMM_XMM {
-    vdivf dest=xmm0, src1=xmm0v, src2=xmmrm, size=8, VL=16
-    vclear dest=xmm2, destVL=16
-};
-def macroop VMULPD_XMM_M {
-    ldfp128 ufp0, seg, sib, "DISPLACEMENT", dataSize=8
-    vdivf dest=xmm0, src1=xmm0v, src2=ufp0, size=8, VL=16
-    vclear dest=xmm2, destVL=16
-};
-def macroop VMULPD_XMM_XMM_M {
-    ldfp128 ufp0, seg, sib, "DISPLACEMENT", dataSize=8
-    vdivf dest=xmm0, src1=xmm0v, src2=ufp0, size=8, VL=16
-    vclear dest=xmm2, destVL=16
-};
-def macroop VMULPD_XMM_XMM_P {
-    ldfp128 ufp0, seg, sib, "DISPLACEMENT", dataSize=8
-    vdivf dest=xmm0, src1=xmm0v, src2=ufp0, size=8, VL=16
-    vclear dest=xmm2, destVL=16
-};
-def macroop VMULPD_XMM_XMM_XMM_M {
-    ldfp128 ufp0, seg, sib, "DISPLACEMENT", dataSize=8
-    vdivf dest=xmm0, src1=xmm0v, src2=ufp0, size=8, VL=16
-    vclear dest=xmm2, destVL=16
-};
-def macroop VMULPD_XMM_XMM_XMM_P {
-    ldfp128 ufp0, seg, sib, "DISPLACEMENT", dataSize=8
-    vdivf dest=xmm0, src1=xmm0v, src2=ufp0, size=8, VL=16
-    vclear dest=xmm2, destVL=16
-};
-def macroop VMULPD_YMM_YMM {
-    vdivf dest=xmm0, src1=xmm0v, src2=xmmrm, size=8, VL=32
-};
-def macroop VMULPD_YMM_YMM_XMM_XMM {
-    vdivf dest=xmm0, src1=xmm0v, src2=xmmrm, size=8, VL=32
-};
-def macroop VMULPD_YMM_M {
-    ldfp256 ufp0, seg, sib, "DISPLACEMENT", dataSize=8
-    vdivf dest=xmm0, src1=xmm0v, src2=ufp0, size=8, VL=32
-};
-def macroop VMULPD_YMM_YMM_M {
-    ldfp256 ufp0, seg, sib, "DISPLACEMENT", dataSize=8
-    vdivf dest=xmm0, src1=xmm0v, src2=ufp0, size=8, VL=32
-};
-def macroop VMULPD_YMM_YMM_P {
-    ldfp256 ufp0, seg, sib, "DISPLACEMENT", dataSize=8
-    vdivf dest=xmm0, src1=xmm0v, src2=ufp0, size=8, VL=32
-};
-def macroop VMULPD_YMM_YMM_XMM_M {
-    ldfp256 ufp0, seg, sib, "DISPLACEMENT", dataSize=8
-    vdivf dest=xmm0, src1=xmm0v, src2=ufp0, size=8, VL=32
-};
-def macroop VMULPD_YMM_YMM_XMM_P {
-    ldfp256 ufp0, seg, sib, "DISPLACEMENT", dataSize=8
     vdivf dest=xmm0, src1=xmm0v, src2=ufp0, size=8, VL=32
 };
 '''
--- src/arch/x86/isa/insts/avx_subset/double/data_transfer/vbroadcastsd.py
+++ src/arch/x86/isa/insts/avx_subset/double/data_transfer/vbroadcastsd.py
@@ -20,24 +20,5 @@ def macroop VBROADCASTSD_256_XMM_P {
     ldfp256 ufp0, seg, riprel, "DISPLACEMENT", dataSize=8
     vbcastf dest=xmm0, src=ufp0, size=8, VL=32
 };
-def macroop VBROADCASTSD_XMM_M {
-    ldfp128 ufp0, seg, sib, "DISPLACEMENT", dataSize=8
-    vbcastf dest=xmm0, src=ufp0, size=8, VL=16
-    vclear dest=xmm2, destVL=16
-};
-def macroop VBROADCASTSD_XMM_P {
-    rdip t7
-    ldfp128 ufp0, seg, riprel, "DISPLACEMENT", dataSize=8
-    vbcastf dest=xmm0, src=ufp0, size=8, VL=16
-    vclear dest=xmm2, destVL=16
-};
-def macroop VBROADCASTSD_YMM_M {
-    ldfp256 ufp0, seg, sib, "DISPLACEMENT", dataSize=8
-    vbcastf dest=xmm0, src=ufp0, size=8, VL=32
-};
-def macroop VBROADCASTSD_YMM_P {
-    rdip t7
-    ldfp256 ufp0, seg, riprel, "DISPLACEMENT", dataSize=8
-    vbcastf dest=xmm0, src=ufp0, size=8, VL=32
-};
+ 
 '''
