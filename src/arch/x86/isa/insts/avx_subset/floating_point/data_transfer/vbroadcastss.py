2ee628fe39ad66a3600e6524eef5b602f5ad3695
Karthikeya Kotha <kkotha2@wisc.edu>
2025-12-08 16:07:42 -0600

    fixing broadcastss

--- src/arch/x86/isa/insts/avx_subset/floating_point/data_transfer/vbroadcastss.py
+++ src/arch/x86/isa/insts/avx_subset/floating_point/data_transfer/vbroadcastss.py
@@ -2,28 +2,27 @@ microcode = '''
 # Broadcast low 32-bit float to all lanes
 def macroop VBROADCASTSS_128_XMM_XMM {
     vbroadcastf dest=xmm0, src1=xmmrm, size=4, VL=16
-    vclear dest=xmm2, destVL=16
 };
 def macroop VBROADCASTSS_128_XMM_M {
-    ldfp128 ufp0, seg, sib, "DISPLACEMENT", dataSize=8
+    ldfp ufp0, seg, sib, "DISPLACEMENT", dataSize=4
     vbroadcastf dest=xmm0, src1=ufp0, size=4, VL=16
-    vclear dest=xmm2, destVL=16
 };
 def macroop VBROADCASTSS_128_XMM_P {
-    ldfp128 ufp0, seg, riprel, "DISPLACEMENT", dataSize=8
+    rdip t7
+    ldfp ufp0, seg, riprel, "DISPLACEMENT", dataSize=4
     vbroadcastf dest=xmm0, src1=ufp0, size=4, VL=16
-    vclear dest=xmm2, destVL=16
 };
 
 def macroop VBROADCASTSS_256_XMM_XMM {
     vbroadcastf dest=xmm0, src1=xmmrm, size=4, VL=32
 };
 def macroop VBROADCASTSS_256_XMM_M {
-    ldfp256 ufp0, seg, sib, "DISPLACEMENT", dataSize=8
+    ldfp ufp0, seg, sib, "DISPLACEMENT", dataSize=4
     vbroadcastf dest=xmm0, src1=ufp0, size=4, VL=32
 };
 def macroop VBROADCASTSS_256_XMM_P {
-    ldfp256 ufp0, seg, riprel, "DISPLACEMENT", dataSize=8
+    rdip t7
+    ldfp ufp0, seg, riprel, "DISPLACEMENT", dataSize=4
     vbroadcastf dest=xmm0, src1=ufp0, size=4, VL=32
 };
 '''
