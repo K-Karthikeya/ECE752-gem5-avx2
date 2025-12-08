7c348832ab706d31edd8f79637445b891a9ef137
Karthikeya Kotha <kkotha2@wisc.edu>
2025-12-08 17:34:50 -0600

    Adding new tests for doubles-fixing dups

--- src/arch/x86/isa/insts/avx_subset/double/data_transfer/vmovddup.py
+++ src/arch/x86/isa/insts/avx_subset/double/data_transfer/vmovddup.py
@@ -4,13 +4,13 @@ def macroop VMOVDDUP_128_XMM_XMM {
     vclear dest=xmm2, destVL=16
 };
 def macroop VMOVDDUP_128_XMM_M {
-    ldfp128 ufp0, seg, sib, "DISPLACEMENT", dataSize=8
+    ldfp ufp0, seg, sib, "DISPLACEMENT", dataSize=8
     vdupf dest=xmm0, src=ufp0, lane=0, size=8, VL=16
     vclear dest=xmm2, destVL=16
 };
 def macroop VMOVDDUP_128_XMM_P {
     rdip t7
-    ldfp128 ufp0, seg, riprel, "DISPLACEMENT", dataSize=8
+    ldfp ufp0, seg, riprel, "DISPLACEMENT", dataSize=8
     vdupf dest=xmm0, src=ufp0, lane=0, size=8, VL=16
     vclear dest=xmm2, destVL=16
 };
@@ -18,12 +18,12 @@ def macroop VMOVDDUP_256_XMM_XMM {
     vdupf_half dest=xmm0, src=xmmrm, size=8, VL=32
 };
 def macroop VMOVDDUP_256_XMM_M {
-    ldfp256 ufp0, seg, sib, "DISPLACEMENT", dataSize=8
+    ldfp128 ufp0, seg, sib, "DISPLACEMENT", dataSize=16
     vdupf_half dest=xmm0, src=ufp0, size=8, VL=32
 };
 def macroop VMOVDDUP_256_XMM_P {
     rdip t7
-    ldfp256 ufp0, seg, riprel, "DISPLACEMENT", dataSize=8
+    ldfp128 ufp0, seg, riprel, "DISPLACEMENT", dataSize=16
     vdupf_half dest=xmm0, src=ufp0, size=8, VL=32
 };
 """
