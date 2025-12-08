718020d6de688a4b8854cd4745f4fe938bc24a9e
Karthikeya Kotha <kkotha2@wisc.edu>
2025-12-07 21:35:07 -0600

    Int Testing

--- src/arch/x86/isa/insts/avx_subset/integer/arithmetic/vpbroadcastd.py
+++ src/arch/x86/isa/insts/avx_subset/integer/arithmetic/vpbroadcastd.py
@@ -9,15 +9,17 @@ def macroop VPBROADCASTD_128_XMM_XMM {
 
 # 128-bit width; reg-mem SIB form (VPBROADCASTD reg, [mem])
 def macroop VPBROADCASTD_128_XMM_M {
-    ldfp ufp0, seg, sib, "DISPLACEMENT", dataSize=4
-    vbroadcasti dest=xmm0, src1=ufp0, size=4, VL=16
+    ld t1, seg, sib, "DISPLACEMENT", dataSize=4
+    movfp xmm0, t1, dataSize=4
+    vbroadcasti dest=xmm0, src1=xmm0, size=4, VL=16
 };
 
 # 128-bit width; reg-mem RIP-relative form
 def macroop VPBROADCASTD_128_XMM_P {
     rdip t7
-    ldfp ufp0, seg, riprel, "DISPLACEMENT", dataSize=4
-    vbroadcasti dest=xmm0, src1=ufp0, size=4, VL=16
+    ld t1, seg, riprel, "DISPLACEMENT", dataSize=4
+    movfp xmm0, t1, dataSize=4
+    vbroadcasti dest=xmm0, src1=xmm0, size=4, VL=16
 };
 
 # 256-bit width (VEX.L=1); reg-reg form
@@ -27,14 +29,16 @@ def macroop VPBROADCASTD_256_XMM_XMM {
 
 # 256-bit width; reg-mem SIB form
 def macroop VPBROADCASTD_256_XMM_M {
-    ldfp ufp0, seg, sib, "DISPLACEMENT", dataSize=4
-    vbroadcasti dest=xmm0, src1=ufp0, size=4, VL=32
+    ld t1, seg, sib, "DISPLACEMENT", dataSize=4
+    movfp xmm0, t1, dataSize=4
+    vbroadcasti dest=xmm0, src1=xmm0, size=4, VL=32
 };
 
 # 256-bit width; reg-mem RIP-relative form
 def macroop VPBROADCASTD_256_XMM_P {
     rdip t7
-    ldfp ufp0, seg, riprel, "DISPLACEMENT", dataSize=4
-    vbroadcasti dest=xmm0, src1=ufp0, size=4, VL=32
+    ld t1, seg, riprel, "DISPLACEMENT", dataSize=4
+    movfp xmm0, t1, dataSize=4
+    vbroadcasti dest=xmm0, src1=xmm0, size=4, VL=32
 };
 '''
