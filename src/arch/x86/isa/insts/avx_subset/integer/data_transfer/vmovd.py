70791411130aabc26c5bd7891a813a3d1d4cd6e7
Karthikeya Kotha <kkotha2@wisc.edu>
2025-12-08 13:31:40 -0600

    Fixing vmovd instr

--- src/arch/x86/isa/insts/avx_subset/integer/data_transfer/vmovd.py
+++ src/arch/x86/isa/insts/avx_subset/integer/data_transfer/vmovd.py
@@ -4,34 +4,34 @@ microcode = '''
 
 # Load from GPR to XMM (zeros upper bits)
 def macroop VMOVD_XMM_R {
-    mov2fp xmml, regm, srcSize=dsz, destSize=8
+    mov2fp xmml, regm, srcSize=4, destSize=8
     lfpimm xmmh, 0
 };
 
 # Load from memory to XMM (zeros upper bits)
 def macroop VMOVD_XMM_M {
-    ldfp xmml, seg, sib, disp, dataSize=dsz
+    ldfp xmml, seg, sib, disp, dataSize=4
     lfpimm xmmh, 0
 };
 
 def macroop VMOVD_XMM_P {
     rdip t7
-    ldfp xmml, seg, riprel, disp, dataSize=dsz
+    ldfp xmml, seg, riprel, disp, dataSize=4
     lfpimm xmmh, 0
 };
 
 # Store from XMM to GPR (extracts lower 32 bits)
 def macroop VMOVD_R_XMM {
-    mov2int reg, xmmlm, size=dsz
+    mov2int reg, xmmlm, size=4
 };
 
 # Store from XMM to memory (extracts lower 32 bits)
 def macroop VMOVD_M_XMM {
-    stfp xmml, seg, sib, disp, dataSize=dsz
+    stfp xmml, seg, sib, disp, dataSize=4
 };
 
 def macroop VMOVD_P_XMM {
     rdip t7
-    stfp xmml, seg, riprel, disp, dataSize=dsz
+    stfp xmml, seg, riprel, disp, dataSize=4
 };
 '''
